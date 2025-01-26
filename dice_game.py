aae933e57f39c950da736e6c4a2d136837378fd5
Thatikonda Vishwanath <thatikondavishwanath@gmail.com>
2025-01-26 23:55:08 +0530

    change code in next commit

--- dice_game.py
+++ dice_game.py
@@ -1,13 +1,29 @@
 import random
 def play_game(n):
-    lst = [1,2,3,4,5,6]
+    while n > 0:
+        x = []
+        lst = [1,2,3,4,5,6]
 
 
-    user_lst = []
-    
-    for i in range(n):
-        user_choice = random.choice(lst)
-        user_lst.append(user_choice)
+        user_lst = []
+        
+        for i in range(5):
+            user_choice = random.choice(lst)
+            user_lst.append(user_choice)
+
+        
+
+        x.append(sum(user_lst))
+        
+
+        n -= 1
+
+
+    return x
+
+x = play_game(3)
+print(x)
+
             
 
             
