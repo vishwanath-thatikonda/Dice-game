dc72ab0e08523472ff9d1596616a07e05bf8f966
Thatikonda Vishwanath <thatikondavishwanath@gmail.com>
2025-01-26 23:33:20 +0530

    created a function

---
+++ dice_game.py
@@ -0,0 +1,15 @@
+import random
+def play_game(n):
+    lst = [1,2,3,4,5,6]
+
+
+    user_lst = []
+    
+    for i in range(n):
+        user_choice = random.choice(lst)
+        user_lst.append(user_choice)
+            
+
+            
+
+    
